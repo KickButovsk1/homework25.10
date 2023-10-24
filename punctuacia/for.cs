cbe4f0bd76ca2338b9d143009cef55a85180f157
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-24 23:38:52 +0500

    Add files via upload

---
+++ punctuacia/for.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string pattern = ".,;:!?";
+            int i = 0;
+            for (int k = 0; k<str.Length; k++)
+            {
+                if (pattern.Contains(str[k]))
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(i);
+            Console.ReadKey();
+        }
+    }
+}

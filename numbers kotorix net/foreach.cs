4ff36eac9a753c6ebeccec9b4ef54aabfb80f872
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-25 00:40:35 +0500

    Add files via upload

---
+++ numbers kotorix net/foreach.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string result = String.Empty;
+            string pattern = "1234567890";
+            str = str.ToLower();
+            foreach (char item in str)
+            {
+                if (Char.IsDigit(item))
+                {
+                    if (!result.Contains(item))
+                    {
+                        result += item;
+                    }
+                }
+            }
+            foreach (char item in result)
+            {
+                pattern = pattern.Replace(item.ToString(), String.Empty);
+            }
+            if (pattern == String.Empty)
+            {
+                pattern = "NO";
+            }
+            Console.WriteLine(pattern);
+            Console.ReadKey();
+        }
+    }
+}

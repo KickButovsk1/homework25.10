efaeb1fd6358582585822d59c53c54548d07b619
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-25 00:24:44 +0500

    Add files via upload

---
+++ true name var/работает без for and foreach.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string pattern = "abcdefghijklmnopqrstuwxyz1234567890_";
+            str = str.ToLower();
+            if (Char.IsDigit(str.First()))
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();        
+        }
+    }
+}

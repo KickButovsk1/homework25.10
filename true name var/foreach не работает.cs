fec2eb2c793256196c70a9a9e02c9799a5c16ca6
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-10-25 00:22:09 +0500

    Add files via upload

---
+++ true name var/foreach не работает.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string pattern = "abcdefghijklmnopqrstuwxyz1234567890_";
+            string result = String.Empty;
+            str = str.ToLower();
+            foreach (char item in str)
+            {
+                if (!pattern.Contains(item))
+                {
+                    result += item;
+                }
+            }
+            foreach (char symbol in str) 
+            {
+                if (char.IsDigit(str.First()))
+                {
+                    result += symbol;
+                }
+            }
+            foreach (char final in str)
+            { 
+                if (str.Contains(pattern))
+                {
+                    Console.WriteLine(str);
+                }
+            }
+            Console.ReadKey();        
+        }
+    }
+}
